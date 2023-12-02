d7c491dd292bd4870739bebbd6072cdec1ba0772
Malgorzata Victor <malgorzatavictor@gmail.com>
2023-12-02 22:25:51 +0000

    Add updatePet function

--- src/main/kotlin/controllers/PetAPI.kt
+++ src/main/kotlin/controllers/PetAPI.kt
@@ -19,4 +19,20 @@ class PetAPI {
         return pets.find { pet -> pet.petID == id }
     }
 
+    fun updatePet(indexToUpdate: Int, pet: Pet?): Boolean {
+        val foundPet = findPet(indexToUpdate)
+
+        if ((foundPet != null) && (pet != null)) {
+            foundPet.petID = pet.petID
+            foundPet.name = pet.name
+            foundPet.breed = pet.breed
+            foundPet.DOB = pet.DOB
+            foundPet.isVaccinated = pet.isVaccinated
+            foundPet.vetID = pet.vetID
+            foundPet.PPS = pet.PPS
+            return true
+        }
+
+        return false
+    }
 }
