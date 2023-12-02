c785bbff39a3eb435fd448a0d37a4ba2747c0dea
Malgorzata Victor <malgorzatavictor@gmail.com>
2023-12-02 21:52:38 +0000

    Add Owner class attributes

--- src/main/kotlin/Owner.kt
+++ src/main/kotlin/Owner.kt
@@ -1,2 +1,9 @@
-data class Owner {
-}
+
+
+data class Owner (
+    var PPS: Int,
+    var name: String,
+    var phoneNumber: String,
+    var email: String,
+    var petList: MutableList<Pet> = mutableListOf()
+        )
