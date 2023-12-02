e1f235d08ac37d409c4f6e4e48e8c8d0679ada64
Malgorzata Victor <malgorzatavictor@gmail.com>
2023-12-02 19:58:19 +0000

    Add Pet class attributes

--- src/main/kotlin/Pet.kt
+++ src/main/kotlin/Pet.kt
@@ -1,2 +1,13 @@
-class Pet {
-}
+import java.time.LocalDate
+
+data class Pet (
+    var petID: Int,
+    var name: String,
+    var breed: String,
+    var DOB: LocalDate,
+    var isVaccinated: Boolean,
+    var vetID: Int,
+    var PPS: Int
+
+
+)
