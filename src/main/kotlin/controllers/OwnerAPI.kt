08d7baa2c0b25b36689d3a69865a5d242d4e41fc
Malgorzata Victor <malgorzatavictor@gmail.com>
2023-12-02 22:57:58 +0000

    Add addOwner() method

--- src/main/kotlin/controllers/OwnerAPI.kt
+++ src/main/kotlin/controllers/OwnerAPI.kt
@@ -1,4 +1,12 @@
 package controllers
 
+import models.Owner
+import models.Vet
+
 class OwnerAPI {
+    private var owners = ArrayList<Owner>()
+
+    fun addOwner(owner: Owner): Boolean {
+        return owners.add(owner)
+    }
 }
