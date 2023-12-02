657a32fc8230358ddcba50cbbbc74575e3c58657
Malgorzata Victor <malgorzatavictor@gmail.com>
2023-12-02 21:42:26 +0000

    Override toString() method

--- src/main/kotlin/Vet.kt
+++ src/main/kotlin/Vet.kt
@@ -1,6 +1,6 @@
 import java.time.LocalDate
 
-class Vet (
+class Vet(
     var vetID: Int,
     var name: String,
     var dateQualified: LocalDate,
@@ -8,4 +8,10 @@ class Vet (
     var salary: Double,
     var position: String,
     var patientList: MutableList<Pet> = mutableListOf()
-        )
+) {
+    override fun toString(): String {
+        val patientsInfo = patientList.joinToString("\n") { it.toString() }
+        return "vetID: $vetID, Name: $name, Date Qualified: $dateQualified, Specialisation: $specialisation, Salary: $salary, Position: $position, Patients: $patientList " +
+            "Patients:\n$patientsInfo"
+    }
+}
