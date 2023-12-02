31d2bb7a47332c9c6ee1574151ae889a723cdd3e
Malgorzata Victor <malgorzatavictor@gmail.com>
2023-12-02 22:17:47 +0000

    Add addPet function

--- src/main/kotlin/controllers/PetAPI.kt
+++ src/main/kotlin/controllers/PetAPI.kt
@@ -1,4 +1,15 @@
 package controllers
 
+import models.Pet
+
 class PetAPI {
+    private var pets = ArrayList<Pet>()
+
+
+
+    fun add(pet: Pet): Boolean {
+        return pets.add(pet)
+    }
+
+
 }
