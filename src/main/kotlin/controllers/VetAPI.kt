eec715bab9b29cad1f777a2452105074fefd4efc
Malgorzata Victor <malgorzatavictor@gmail.com>
2023-12-02 22:41:07 +0000

    Add addVet method

--- src/main/kotlin/controllers/VetAPI.kt
+++ src/main/kotlin/controllers/VetAPI.kt
@@ -1,10 +1,13 @@
 package controllers
 
 
+import models.Pet
 import models.Vet
 
 class VetAPI {
     private var vets = ArrayList<Vet>()
 
-
+    fun addVet(vet: Vet): Boolean {
+        return vets.add(vet)
+    }
 }
