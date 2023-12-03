4d1f50de752da86deb247131914e41c3dfaa1368
Malgorzata Victor <malgorzatavictor@gmail.com>
2023-12-03 12:09:16 +0000

    Add loadAll() & saveAll() to Main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,34 @@
+import controllers.OwnerAPI
+import controllers.PetAPI
+import controllers.VetAPI
+import persistence.XMLSerializer
+import java.io.File
+
+private val petAPI = PetAPI(XMLSerializer(File("pets.xml")))
+private val vetAPI = VetAPI(XMLSerializer(File("vets.xml")))
+private val ownerAPI = OwnerAPI(XMLSerializer(File("owners.xml")))
+
 fun main(args: Array<String>) {
     println("models.Vet System V1.0")
     println("test")
+}
+
+fun saveAll() {
+    try {
+        petAPI.savePets()
+        vetAPI.saveVets()
+        ownerAPI.saveOwners()
+    } catch (e: Exception) {
+        System.err.println("Error writing to file: $e")
+    }
+}
+
+fun loadAll() {
+    try {
+        petAPI.loadPets()
+        vetAPI.loadVets()
+        ownerAPI.loadOwners()
+    } catch (e: Exception) {
+        System.err.println("Error reading from file: $e")
+    }
 }
