cb32a71ed1a128820a0e84e2c293c74177a07a6a
Malgorzata Victor <malgorzatavictor@gmail.com>
2023-12-03 13:31:08 +0000

    Add mainMenu & runMainMenu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ import controllers.OwnerAPI
 import controllers.PetAPI
 import controllers.VetAPI
 import persistence.XMLSerializer
+import utils.ScannerInput
 import java.io.File
 
 private val petAPI = PetAPI(XMLSerializer(File("pets.xml")))
@@ -9,8 +10,37 @@ private val vetAPI = VetAPI(XMLSerializer(File("vets.xml")))
 private val ownerAPI = OwnerAPI(XMLSerializer(File("owners.xml")))
 
 fun main(args: Array<String>) {
-    println("models.Vet System V1.0")
-    println("test")
+    loadAll()
+    runMainMenu()
+}
+
+fun mainMenu(): Int {
+    return ScannerInput.readNextInt(
+        """ 
+             
+        ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+        ┃            Vet System             ┃
+        ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+        ┃  1) Pet Menu                      ┃
+        ┃  2) Vet Menu                      ┃
+        ┃  3) Owner Menu                    ┃
+        ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+        ┃  0) Exit                          ┃
+        ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+        Enter option➡️"""
+    )
+}
+
+fun runMainMenu() {
+    do {
+        when (val option = mainMenu()) {
+            1 -> petMenu()
+            2 -> vetMenu()
+            3 -> ownerMenu()
+            0 -> exitApp()
+            else -> println("Invalid option entered: $option")
+        }
+    } while (true)
 }
 
 fun saveAll() {
