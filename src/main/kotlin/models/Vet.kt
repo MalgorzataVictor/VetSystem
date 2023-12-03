1e85898ecc20e580e5766bb1a0d16ae6fbd19e6f
Malgorzata Victor <malgorzatavictor@gmail.com>
2023-12-03 16:12:40 +0000

    Add formatListStringSpecialisation() in Vet

--- src/main/kotlin/models/Vet.kt
+++ src/main/kotlin/models/Vet.kt
@@ -11,6 +11,16 @@ data class Vet(
     var position: String,
     var patientList: MutableList<Pet> = mutableListOf()
 ) {
+    fun formatListStringSpecialisation(): String =
+        specialisation
+            .joinToString(separator = "\n") { spec ->
+                "" + specialisation.indexOf(spec).toString() + ": " + spec
+            }
+    fun formatListStringPatientList(): String =
+        patientList
+            .joinToString(separator = "\n") { pat ->
+                "" + patientList.indexOf(pat).toString() + ": " + pat
+            }
     override fun toString(): String {
         val patientsInfo = patientList.joinToString("\n") { it.toString() }
         return "vetID: $vetID, Name: $name, Date Qualified: $dateQualified, Specialisation: $specialisation, Salary: $salary, Position: $position, Patients: $patientList " +
