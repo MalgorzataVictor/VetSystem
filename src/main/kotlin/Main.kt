50b163ef220f6a203342acd393cc66c998b975d1
Malgorzata Victor <malgorzatavictor@gmail.com>
2023-12-03 13:39:13 +0000

    Add petMenu & runPetMenu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,7 @@ import controllers.VetAPI
 import persistence.XMLSerializer
 import utils.ScannerInput
 import java.io.File
+import kotlin.system.exitProcess
 
 private val petAPI = PetAPI(XMLSerializer(File("pets.xml")))
 private val vetAPI = VetAPI(XMLSerializer(File("vets.xml")))
@@ -27,17 +28,52 @@ fun mainMenu(): Int {
         ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
         ┃  0) Exit                          ┃
         ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-        Enter option➡️"""
+        Enter option️"""
     )
 }
 
 fun runMainMenu() {
     do {
         when (val option = mainMenu()) {
-            1 -> petMenu()
-            2 -> vetMenu()
-            3 -> ownerMenu()
+            1 -> runPetMenu()
+            2 -> runVetMenu()
+            3 -> runOwnerMenu()
             0 -> exitApp()
+            else -> println("Invalid option entered: $option")
+        }
+    } while (true)
+}
+
+fun petMenu(): Int {
+    return ScannerInput.readNextInt(
+        """ 
+             
+        ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+        ┃            Pet                    ┃
+        ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+        ┃  1) Add Pet                       ┃
+        ┃  2) Delete Pet                    ┃
+        ┃  3) List Pet                      ┃ 
+        ┃  4) Update Pet                    ┃
+        ┃  5) Number Of Pet                 ┃
+        ┃  6) Search Pet                    
+        ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+        ┃  0) Exit                          ┃
+        ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+        Enter option️"""
+    )
+}
+
+fun runPetMenu() {
+    do {
+        when (val option = petMenu()) {
+            /*   1 -> //addPet()
+             2 -> deletePet()
+              3 -> listAllPets()
+              4 -> updatePet()
+              5 -> numberOfPets()
+              6 -> searchPet
+              0 -> exitApp()*/
             else -> println("Invalid option entered: $option")
         }
     } while (true)
@@ -61,4 +97,10 @@ fun loadAll() {
     } catch (e: Exception) {
         System.err.println("Error reading from file: $e")
     }
+}
+fun exitApp() {
+    saveAll()
+    println()
+    print("Exiting...")
+    exitProcess(0)
 }
